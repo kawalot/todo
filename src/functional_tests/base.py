3b90ef4b30949dbed1b3ac15c9f2309bb82b8ebb
AleksandrPeshcherov <kawalot@gmail.com>
2019-02-15 17:16:16 +0200

    add @wait decorator

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -16,6 +16,19 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def wait(fn):
+        def modified_fn(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+        return modified_fn
+
+    @wait
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -29,6 +42,7 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
     
+    @wait
     def wait_for(self, fn):
         start_time = time.time()
         while True:
@@ -42,13 +56,15 @@ class FunctionalTest(StaticLiveServerTestCase):
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
 
+    @wait
     def wait_to_be_logged_in(self, email):
         self.wait_for(
             lambda: self.browser.find_element_by_link_text('Log out')
         )
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
-        
+
+    @wait
     def wait_to_be_logged_out(self, email):
         self.wait_for(
             lambda: self.browser.find_element_by_name('email')
