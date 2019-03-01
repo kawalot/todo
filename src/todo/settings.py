46fc63a825b54cb96d571397cafc866586ac7080
AleksandrPeshcherov <kawalot@gmail.com>
2019-03-01 09:24:36 +0200

    book end

--- src/todo/settings.py
+++ src/todo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'lists',
     'accounts',
+    'rest_framework',
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
--- src/todo/urls.py
+++ src/todo/urls.py
@@ -3,12 +3,14 @@ from django.urls import include, path
 from lists import views as list_views
 from lists import urls as list_urls
 from accounts import urls as accounts_urls
-from lists import api_urls
+# from lists import api_urls
+from lists.api import router
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', list_views.home_page, name='home'),
     path('lists/', include(list_urls)),
     path('accounts/', include(accounts_urls)),
-    path('api/', include(api_urls)),
+    #path('api/', include(api_urls)),
+    path('api/', include(router.urls)),
 ]
